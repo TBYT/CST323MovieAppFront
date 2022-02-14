fbb1b2dbf3a9ff4ee73ec0581c69b35fd65cdc4e
TBYT <TBYTBiz@googlegroups.com>
2022-02-13 20:17:46 -0700

    fixed path
    1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CST323MovieApp'`, () => {
+  it(`should have as title 'CST323MovieAppFrontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CST323MovieApp');
+    expect(app.title).toEqual('CST323MovieAppFrontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('CST323MovieApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CST323MovieAppFrontend app is running!');
   });
 });
