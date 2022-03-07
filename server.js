187c7aca6c64a5f992510dad98fd22d74b11439d
TBYT <TBYTBiz@googlegroups.com>
2022-03-06 19:05:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const port = (process.env.PORT || 8080);
 const app = express();
 
 app.use(express.static(__dirname));
@@ -7,7 +8,10 @@ app.use(express.static(__dirname + '/dist/cst323movieappfrontend'));
 
 app.get('/*', function(req,res) 
 {
-    res.sendFile(path.join(__dirname+'/dist/cst323movieappfrontend/index.html'));
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, function() 
+{
+    console.info("Angular App is listening!");
+});
