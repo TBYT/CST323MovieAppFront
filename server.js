53919108b7575457321bf06c9415a444a908d067
TBYT <TBYTBiz@googlegroups.com>
2022-03-06 18:56:22 -0700

    Update server.js
    configured 2

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/dist/cst323movieappfrontend'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/cst323movieappfrontend/index.html'));});
+
+app.get('/*', function(req,res) 
+{
+    res.sendFile(path.join(__dirname+'/dist/cst323movieappfrontend/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
