6ca3d1172173885e852dcc687f757abc35ce3cba
TBYT <TBYTBiz@googlegroups.com>
2022-03-10 13:10:47 -0700

    Update server.js
    capitalize

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = (process.env.PORT || 8080);
 const app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/dist/cst323movieappfrontend'));
+app.use(express.static(__dirname + '/dist/CST323MovieAppFrontend'));
 
 app.get('/ping', function (req, res) {
     return res.send('pong port: '+port);
@@ -12,10 +12,10 @@ app.get('/ping', function (req, res) {
 
 app.get('/*', function(req,res) 
 {
-    res.sendFile(path.join(__dirname+'/dist/cst323movieappfrontend/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/CST323MovieAppFrontend/index.html'));
 });
 
 app.listen(port, function() 
 {
     console.info("Angular App is listening!");
-})
+});
